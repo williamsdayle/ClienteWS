e2c7bc6a89889655923618fff4619a4c50d7ac6b
William Sdayle <williamsdy30@gmail.com>
2018-05-09 16:01:01 -0300

    Add files via upload

---
+++ ClienteWS/build/generated/jax-wsCache/ImplService/org/clientews/Correntista.java
@@ -0,0 +1,114 @@
+
+package org.clientews;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java de correntista complex type.
+ * 
+ * <p>O seguinte fragmento do esquema especifica o conteúdo esperado contido dentro desta classe.
+ * 
+ * <pre>
+ * &lt;complexType name="correntista">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="codigo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="nome" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="saldo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "correntista", propOrder = {
+    "codigo",
+    "nome",
+    "saldo"
+})
+public class Correntista {
+
+    protected String codigo;
+    protected String nome;
+    protected String saldo;
+
+    /**
+     * Obtém o valor da propriedade codigo.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * Define o valor da propriedade codigo.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCodigo(String value) {
+        this.codigo = value;
+    }
+
+    /**
+     * Obtém o valor da propriedade nome.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Define o valor da propriedade nome.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNome(String value) {
+        this.nome = value;
+    }
+
+    /**
+     * Obtém o valor da propriedade saldo.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * Define o valor da propriedade saldo.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSaldo(String value) {
+        this.saldo = value;
+    }
+
+}
---
+++ ClienteWS/build/generated/jax-wsCache/ImplService/org/clientews/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://inter.org/")
+package org.clientews;
